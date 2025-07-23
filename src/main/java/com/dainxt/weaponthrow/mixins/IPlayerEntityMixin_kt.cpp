f3a2db9fea579cd3f6d214184423aa7bdd926a6f
Vergesst <3224171967@qq.com>
2025-07-23 15:29:12 +0800

    deleted deprecated files

--- src/main/java/com/dainxt/weaponthrow/mixins/IPlayerEntityMixin_kt.cpp
+++
@@ -1,7 +0,0 @@
-import com.dainxt.weaponthrow.capabilities.PlayerThrowData
-
-interface IPlayerEntityMixin {
-    fun setThrowPower(value: PlayerThrowData)
-
-    fun getThrowPower(): PlayerThrowData
-}
